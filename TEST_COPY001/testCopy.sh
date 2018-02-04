adb5092855bdfdf596659501b6169632b63f0429
cyouraku <tim.zhang1981@gmail.com>
2018-02-05 01:22:28 +0900

    1.add a param to testCopy.sh on 1:23 2018/2/5 Yokohama.Japan

--- TEST_COPY001/testCopy.sh
+++ TEST_COPY001/testCopy.sh
@@ -4,19 +4,21 @@
 DATA_SOURCE="data_if.dat"
 #set output file
 DATA_LOAD="data_copy.data"
+#set max records
+MAX_COUNT=$1
 
 echo "test copy start!"
 
 #clear data output file
 cat /dev/null > ${DATA_LOAD}
 
-int=0
-while [ $int -lt 1000 ]
+int=${MAX_COUNT}
+while [ $int -gt 0 ]
 #echo $int
 do
   cat ${DATA_SOURCE} >> ${DATA_LOAD}
   echo $int
-  let int+=1
+  let int-=1
 done
 
 echo "test copy finished!"
